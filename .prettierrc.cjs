36da24f2e5c63f5f53dfbe50bfd77d8727b36e41
Renato <renatoperezc@gmail.com>
2022-11-16 20:46:11 -0600

    Instal tailwind and config automatic class sorting with prettier

---
+++ .prettierrc.cjs
@@ -0,0 +1,8 @@
+module.exports = {
+  trailingComma: "none",
+  semi: true,
+  singleQuote: true,
+  printWidth: 90,
+  tabWidth: 2,
+  endOfLine: "auto",
+  plugins: [require('prettier-plugin-tailwindcss')]}
